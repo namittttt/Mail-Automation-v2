b0eebb944ff07e18dfc00e6013513489a4b81fb4
Namit Mirajkar <namit@mail.namitsdomain.com>
2026-07-02 14:39:20 +0530

    Add common role for system preparation and package installation

--- sieve-config.sh
+++ sieve-config.sh
@@ -1,12 +1,22 @@
+
 #!/bin/bash
 
 echo "[1/4] Installing Sieve packages..."
 sudo apt install -y dovecot-sieve dovecot-managesieved roundcube-plugins
 
 echo "[2/4] Uncommenting sieve in dovecot config files..."
-sudo sed -i 's/^[[:space:]]*#[[:space:]]*mail_plugins = \$mail_plugins sieve/mail_plugins = $mail_plugins sieve/' /etc/dovecot/conf.d/20-lmtp.conf
+
+# Uncomment mail_plugins { and sieve = yes in 20-imap.conf
+sudo sed -i 's/^[[:space:]]*#[[:space:]]*mail_plugins {/mail_plugins {/' /etc/dovecot/conf.d/20-imap.conf
 sudo sed -i 's/^[[:space:]]*#[[:space:]]*sieve = yes/sieve = yes/' /etc/dovecot/conf.d/20-imap.conf
+
+# Uncomment mail_plugins in 20-lmtp.conf
+sudo sed -i 's/^[[:space:]]*#[[:space:]]*mail_plugins = \$mail_plugins sieve/mail_plugins = $mail_plugins sieve/' /etc/dovecot/conf.d/20-lmtp.conf
+sudo sed -i 's/^[[:space:]]*#[[:space:]]*mail_plugins {/mail_plugins {/' /etc/dovecot/conf.d/20-lmtp.conf
 sudo sed -i 's/^[[:space:]]*#[[:space:]]*sieve = yes/sieve = yes/' /etc/dovecot/conf.d/20-lmtp.conf
+
+# Uncomment mail_plugins { and sieve = yes in 15-lda.conf
+sudo sed -i 's/^[[:space:]]*#[[:space:]]*mail_plugins {/mail_plugins {/' /etc/dovecot/conf.d/15-lda.conf
 sudo sed -i 's/^[[:space:]]*#[[:space:]]*sieve = yes/sieve = yes/' /etc/dovecot/conf.d/15-lda.conf
 
 echo "[3/4] Enabling roundcube managesieve plugin..."
