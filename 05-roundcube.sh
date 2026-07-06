83f228c822decafb89696699c4f512f7cf828487
Namit Mirajkar <namit@mail.namitsdomain.com>
2026-07-06 13:02:53 +0530

    Add common role for system preparation and package installation

--- 05-roundcube.sh
+++ 05-roundcube.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 source /opt/mailserver/mailserver.conf
@@ -13,8 +12,19 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+if [ -f /opt/mailserver-install.tmp ]; then
+    source /opt/mailserver-install.tmp
+else
+    echo "ERROR: Run 01-install.sh first."
+    exit 1
+fi
+
 echo
-echo "[1/5] Configuring Roundcube..."
+echo "[1/6] Generating DES key..."
+DES_KEY=$(pwgen -s 24 1)
+
+echo
+echo "[2/6] Writing Roundcube configuration..."
 
 cat > /etc/roundcube/config.inc.php <<EOF
 <?php
@@ -23,75 +33,112 @@ cat > /etc/roundcube/config.inc.php <<EOF
 
 include('/etc/roundcube/debian-db-roundcube.php');
 
-\$config['imap_host'] = 'localhost:143';
+/* ---------------- IMAP ---------------- */
 
-\$config['smtp_host'] = 'localhost:25';
+\$config['imap_host'] = 'tls://mail.namit.com:143';
+
+\$config['imap_conn_options'] = [
+    'ssl' => [
+        'verify_peer'       => false,
+        'verify_peer_name'  => false,
+        'allow_self_signed' => true,
+    ],
+];
+
+/* ---------------- SMTP ---------------- */
+
+\$config['smtp_host'] = 'tls://mail.namit.com';
+\$config['smtp_port'] = 587;
 
 \$config['smtp_user'] = '%u';
-
 \$config['smtp_pass'] = '%p';
 
-\$config['support_url'] = '';
+\$config['smtp_conn_options'] = [
+    'ssl' => [
+        'verify_peer'       => false,
+        'verify_peer_name'  => false,
+        'allow_self_signed' => true,
+    ],
+];
 
-\$config['product_name'] = 'Namit Mail';
+/* ---------------- UI ---------------- */
 
-\$config['des_key'] = 'voC5fbUK9IgBGAU148NC91ap';
+\$config['product_name'] = 'Mail';
+\$config['des_key'] = '${DES_KEY}';
+\$config['skin'] = 'elastic';
+\$config['language'] = 'en_US';
+\$config['enable_spellcheck'] = false;
+
+\$config['quota_zero_as_unlimited'] = false;
+
+/* ---------------- Plugins ---------------- */
 
 \$config['plugins'] = [
     'archive',
-    'zipdownload',
+    'zipdownload'
 ];
 
-\$config['skin'] = 'elastic';
+/* ---------------- Logging ---------------- */
 
-\$config['enable_spellcheck'] = false;
+\$config['log_driver'] = 'file';
+\$config['debug_level'] = 4;
+\$config['smtp_debug'] = true;
+\$config['smtp_log'] = true;
 
-\$config['language'] = 'en_US';
 EOF
 
 echo
-echo "[2/5] Configuring Apache Virtual Host..."
+echo "[3/6] Importing Roundcube database..."
+
+if ! mysql roundcube -e "SHOW TABLES;" | grep -q users; then
+    mysql roundcube < /usr/share/roundcube/SQL/mysql.initial.sql
+fi
+
+echo
+echo "[4/6] Configuring Apache..."
 
 cat > /etc/apache2/sites-available/roundcube.conf <<EOF
 <VirtualHost *:80>
 
-    ServerName $MAILHOST
+ServerName mail.namit.com
 
-    DocumentRoot /usr/share/roundcube
+DocumentRoot /usr/share/roundcube
 
-    <Directory /usr/share/roundcube>
-        Options FollowSymLinks
-        AllowOverride All
-        Require all granted
-    </Directory>
+<Directory /usr/share/roundcube>
+Options FollowSymLinks
+AllowOverride All
+Require all granted
+</Directory>
 
-    ErrorLog \${APACHE_LOG_DIR}/roundcube-error.log
-    CustomLog \${APACHE_LOG_DIR}/roundcube-access.log combined
+ErrorLog \${APACHE_LOG_DIR}/roundcube-error.log
+CustomLog \${APACHE_LOG_DIR}/roundcube-access.log combined
 
 </VirtualHost>
 EOF
 
 echo
-echo "[3/5] Enabling Roundcube Site..."
+echo "[5/6] Enabling Apache..."
 
-a2ensite roundcube.conf >/dev/null 2>&1 || true
-
-echo
-echo "[4/5] Testing Apache Configuration..."
+a2enmod rewrite ssl headers >/dev/null
+a2ensite roundcube.conf >/dev/null
+a2dissite 000-default.conf >/dev/null 2>&1 || true
 
 apache2ctl configtest
 
 echo
-echo "[5/5] Restarting Apache..."
+echo "[6/6] Restarting Apache..."
 
 systemctl restart apache2
 systemctl enable apache2
 
+rm -f /opt/mailserver-install.tmp
+
 echo
 echo "========================================"
-echo " Roundcube Configuration Complete"
+echo " Roundcube Installed"
 echo "========================================"
 
 echo
-echo "Roundcube URL:"
-echo "http://$MAILHOST"
+echo "URL  : http://mail.namit.com"
+echo "IMAP : tls://mail.namit.com:143"
+echo "SMTP : tls://mail.namit.com:587"
